a1d170dbebf3fd2a0ea721b9ebceee126900077e
felip <felip@Felipe>
2018-05-21 01:27:56 -0500

    9 commit

--- zoodog.sql
+++ zoodog.sql
@@ -1,9 +1,6 @@
-drop database if exists zoodog;
-Create database if not exists zoodog;
-use zoodog;
 
 create table Dog(
-id int auto_increment not null,
+id serial not null,
 name varchar(100) not null,
 gro varchar(100) not null,
 personality varchar(100) not null,
@@ -13,9 +10,9 @@ image varchar(250) not null,
 primary key(id)
 );
 
-insert into dog (name,gro,personality,energylevel,description,image) values ('labrador','blanco','active','high','beauty','https://www.webconsultas.com/sites/default/files/styles/encabezado_articulo/public/temas/caracteristicas-perro-labrador.jpg?itok=ANTovkRk')
+insert into dog (name,gro,personality,energylevel,description,image) values ('labrador','blanco','active','high','beauty','https://www.webconsultas.com/sites/default/files/styles/encabezado_articulo/public/temas/caracteristicas-perro-labrador.jpg?itok=ANTovkRk');
 create table Comentario(
-id int auto_increment not null,
+id serial not null,
 autor varchar(100) not null,
 fecha Date not null,
 comentario varchar(100) not null,
@@ -23,3 +20,6 @@ id_dog int not null,
 primary key(id),
 foreign key(id_dog) references Dog (id)
 );
+select * from dog;
+
+insert into dog (name,gro,personality,energylevel,description,image) values ('Golden Retriever','herding','smart','very active','beauty','https://t1.ea.ltmcdn.com/es/razas/5/5/0/img_55_golden-retriever-o-cobrador-dorado_0_orig.jpg');
